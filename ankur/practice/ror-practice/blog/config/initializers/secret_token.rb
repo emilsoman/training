52ecadad27375ebf0f14c884528f7aa531e5ba26
Ankur Omer <ankur@neevtech.com>
2012-09-05 20:04:40 +0530

    1. Create a sample RoR project named blog. 2. Initial commit with working project.

---
+++ ankur/practice/ror-practice/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '3cd81b9075177dd6717bd316726b6f2ec88a6c6ec33891815c3ea5b513f3bec9161986cabc7daeae9353d3968e7dfa822ca9ab2280e7740b96ca859a510b7bfb'
